72855d343a3e8570b69866c14e09feb5d99f5ce0
Kaiduev <kaiduevbaktygul07@gmail.com>
2021-04-03 08:45:59 +0600

    Added comments to the functions

--- blog/mixins.py
+++ blog/mixins.py
@@ -9,18 +9,21 @@ from .serializers import FanSerializer
 class LikedMixin:
     @action(detail=True, methods=['POST'])
     def like(self, request, pk=None):
+        """Like a model instance."""
         obj = self.get_object()
         services.add_like(obj, request.user)
         return Response({'message': 'post liked', 'code': 200}, status=status.HTTP_200_OK)
 
     @action(detail=True, methods=['POST'])
     def unlike(self, request, pk=None):
+        """Unlike a model instance."""
         obj = self.get_object()
         services.remove_like(obj, request.user)
         return Response({'message': 'post unliked', 'code': 200}, status=status.HTTP_200_OK)
 
     @action(detail=True, methods=['GET'])
     def get_fans(self, request, pk=None):
+        """Get the users which liked a model instance."""
         obj = self.get_object()
         fans = services.get_fans(obj)
         serializer = FanSerializer(fans, many=True)
